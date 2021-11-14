7cf20e9b30950ae358b1e6bcc6eae42272175916
thisisvignesh <vigneshkvm96@gmail.com>
2021-11-14 12:40:33 +0530

    first

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "ecl_security_host_based_v1" "" {
+  dsm_lang              = ""
+  mail_address          = ""
+  max_agent_value       = 0
+  service_order_service = ""
+  tenant_id             = ""
+  time_zone             = ""
+}
